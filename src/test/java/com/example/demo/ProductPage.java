ff997d6e80a69e3d2993284c9ac8cfb02d803a52
Invorkel1337 <semen03112001@list.ru>
2024-05-08 01:24:35 +0800

    ky

--- src/test/java/com/example/demo/ProductPage.java
+++ src/test/java/com/example/demo/ProductPage.java
@@ -1,56 +1,16 @@
 package com.example.demo;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
+
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
-
-import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // page_url = https://vk.com/club225299895?w=product-225299895_10044406
 public class ProductPage {
-    @BeforeAll
-    public static void setUpAll() {
-        Configuration.browserSize = "1280x800";
-    }
+private SelenideElement
+        PhotoPlaceBig = $(".ItemGallery__main"),
+        PhotoPlaceSmall = $(".vkuiImageBase__img"),
+        ItemName = $(".ItemName"),
+        ItemPrice = $(".MarketItemCard__price");
 
-    @BeforeEach
-    public void setUp() {
-        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
-        open("https://vk.com/club225299895?w=");
-    }
-    @Test
-    public void testClickGroupTabButton(){
-        SelenideElement writtenButton = $(".group-tab-buttons");
-        writtenButton.scrollTo();
-        writtenButton.click();
-    }
-    @Test
-    public void testPhotoPlaces(){
-        List<SelenideElement> photoPlaces = $$(".redesigned-group-avatar, .redesigned-group-cover");
-        assertEquals(2,photoPlaces.size());
-    }
-    @Test
-    public void testButtonText(){
-        SelenideElement subscribeButton =$("#join_button");
-        subscribeButton.shouldHave(Condition.text("подписаться"));
-        SelenideElement moreButton = $(".FlatButton.FlatButton--secondary.FlatButton--size-m.redesigned-group-action.redesigned-group-action--more");
-        moreButton.shouldHave(Condition.text("Ещё"));
-        SelenideElement showAllButton = $(".group-tab-buttons");
-        String initialText = showAllButton.getText();
-        showAllButton.shouldHave(Condition.text("Показать все"));
-        ///После добавление в список улсг
-        showAllButton =$(".group-tab-buttons");
-        String newText = showAllButton.getText();
-        assertEquals(initialText,newText);
-        showAllButton.shouldHave(Condition.text("Показать все"));
-    }
 }
--- src/test/java/com/example/demo/ProductPageTest.java
+++ src/test/java/com/example/demo/ProductPageTest.java
@@ -19,38 +19,4 @@ public class ProductPageTest {
         open("https://vk.com/club225299895?w=product-225299895_10044406");
     }
 
-    @Test
-    public void testClickWriteButton(){
-        SelenideElement writtenButton = $(".MarketServiceButton__text");
-        writtenButton.click();
-        sleep(1000);
-        SelenideElement chatMessage = $("#mail_box_editable");
-        chatMessage.shouldHave(Condition.text("Здравствуйте!Меня заинтересовала эта услуга."));
-    }
-    @Test
-    public void testButtonTest(){
-        SelenideElement subscribeButton = $(".Button-module__root--enpNU.MarketItemCardShopInfo__buttonToggleSubscribeGroup.vkuiButton");
-        subscribeButton.shouldHave(Condition.text("Подписаться"));
-        SelenideElement goToStoreButton = $(".Button-module__root--enpNU.MarketItemCardShopInfo__buttonGoToShop.vkuiButton");
-        goToStoreButton.shouldHave(Condition.text("Перейти в магазин"));
-        SelenideElement writeButton = $(".Button-module__root--enpNU.vkuiButton");
-        writeButton.shouldHave(Condition.text("Написать"));
-    }
-    @Test
-    public void testPhotoPresence(){
-        SelenideElement photo = $(".ItemGallery__main");
-        photo.shouldBe(Condition.visible);
-    }
-    @Test
-    public void testLikeButtonColor(){
-        SelenideElement likeButton = $("._like_button_icon ");
-        likeButton.click();
-        likeButton.shouldHave(Condition.cssValue("color","#ff5c5c"));
-    }
-    @Test
-    public void testButtonColor(){
-        SelenideElement iconButton = $(".vkuiIcon.vkuiIcon--28.vkuiIcon--w-28.vkuiIcon--h-28.vkuiIcon--add_square_outline_28");
-        iconButton.click();
-        iconButton.shouldHave(Condition.cssValue("color","#828282"));
-    }
 }
